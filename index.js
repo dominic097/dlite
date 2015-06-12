d9a3c5a723b2a1b2385656a7218a529ad10d2a3a
Dominic <dominic097@gmail.com>
2015-06-12 19:23:43 +0530

    express js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/docs'));
 
 app.get('/', function(request, response) {
-  response.send('hello');
+  response.sendfile('./docs/doxx_docs/index.html');
 });
 
 app.listen(app.get('port'), function() {
