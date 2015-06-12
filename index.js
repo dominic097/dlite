854432b3d311cb6eb228764a9ade318e3a4712b1
Dominic <dominic097@gmail.com>
2015-06-12 20:35:46 +0530

    path fix

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+	app = express(),
+	path = require('path');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/docs'));
 
 app.get('/', function(request, response) {
-  response.sendfile('./docs/doxx_docs/index.html');
+	response.sendFile('index.html', { root: path.join(__dirname, '/docs/doxx_docs' )});
+  // response.sendFile('./docs/doxx_docs/index.html');
+});
+
+app.get('/*', function(request, response) {
+	console.log(request.url);
+	response.sendFile(request.url, { root: path.join(__dirname, '/docs/doxx_docs' )});
+	// response.sendFile(request.url, { root: __dirname + '/docs/doxx_docs' });
+  
 });
 
 app.listen(app.get('port'), function() {
