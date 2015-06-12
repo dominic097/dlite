893742736082d9bd064b3e43247942485246ffbd
Dominic <dominic097@gmail.com>
2015-06-12 19:48:51 +0530

    path change

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/docs'));
 app.use(express.errorHandler());
 app.get('/', function(request, response) {
-  response.sendfile('./docs/doxx_docs/index.html');
+  response.sendfile('/doxx_docs/index.html');
 });
 
 app.listen(app.get('port'), function() {
